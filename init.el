784d6b85fa099f98ef94949305f5e2502e55d6f1
Aria <blackstab1337@gmail.com>
2016-12-29 00:43:21 +0800

    Changed mail

--- init.el
+++ init.el
@@ -7,3 +7,17 @@
 
 (require 'org)
 (org-babel-load-file (expand-file-name "journal.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (wanderlust flycheck-gometalinter flycheck-gometalinker go-eldoc go-autocomplete exec-path-from-shell go-mode web-mode solarized-theme ruby-end rainbow-delimiters powerline org-bullets magit linum-relative helm-projectile flymake-ruby evil-surround evil-org evil-mu4e emmet-mode company better-defaults android-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
