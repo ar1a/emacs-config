ab7471090858833ae9ed3abee05abb270bb120b6
Aria <blackstab1337@gmail.com>
2017-01-29 16:26:19 +0800

    Added C++ Completion with irony's company backend and binds.

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (wanderlust flycheck-gometalinter flycheck-gometalinker go-eldoc go-autocomplete exec-path-from-shell go-mode web-mode solarized-theme ruby-end rainbow-delimiters powerline org-bullets magit linum-relative helm-projectile flymake-ruby evil-surround evil-org evil-mu4e emmet-mode company better-defaults android-mode))))
+    (company-irony wanderlust flycheck-gometalinter flycheck-gometalinker go-eldoc go-autocomplete exec-path-from-shell go-mode web-mode solarized-theme ruby-end rainbow-delimiters powerline org-bullets magit linum-relative helm-projectile flymake-ruby evil-surround evil-org evil-mu4e emmet-mode company better-defaults android-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
